fc896091a79f1420dffa179299bd622aafd4f357
Vipul Dessai <badgamerbad@gmail.com>
2025-09-12 15:26:07 +0530

    vid 0 - sculpt tools plan updated, refactoring

--- src/learn-topic-wise/01-hash/012-brick-wall.ts
+++ src/learn-topic-wise/01-hash/012-brick-wall.ts
@@ -4,15 +4,22 @@ function leastBricks(wall: number[][]): number {
 
   for (let i = 0; i < wall.length; i++) {
     const row = wall[i];
+
+    // finding the prefix sum is finding the edge
     let pSum = 0;
     for (let j = 0; j < row.length - 1; j++) {
       pSum += row[j];
 
+      // find the max occuring edge
       let cur = m.get(pSum) || 0;
       m.set(pSum, ++cur);
       max = Math.max(max, cur);
     }
   }
 
+  // ask is draw a line such that cross the least bricks
+  // which is same as saying draw a line that passes through max number of edges
+  // `max` represents a point where max edges are there
+  // `wall.length - max` gives the bricks which are crossed
   return wall.length - max;
 }
