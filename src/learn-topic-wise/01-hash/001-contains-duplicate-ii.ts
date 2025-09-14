43bce3a0958cb2ad218d512a8ec601acdefaf15f
Vipul Dessai <badgamerbad@gmail.com>
2025-09-14 14:58:53 +0530

    dsa

--- src/learn-topic-wise/01-hash/001-contains-duplicate-ii.ts
+++ src/learn-topic-wise/01-hash/001-contains-duplicate-ii.ts
@@ -25,3 +25,46 @@ var input = [1, 2, 3, 1],
 var out = containsNearbyDuplicate(input, k);
 
 console.log(out);
+
+function containsNearbyDuplicate_sliding_window(
+  nums: number[],
+  k: number,
+): boolean {
+  const uSet = new Set(),
+    n = nums.length;
+
+  let l = 0,
+    r = 0;
+  while (r <= k && r < n) {
+    const cur = nums[r];
+    if (uSet.has(cur)) {
+      return true;
+    }
+
+    uSet.add(cur);
+    ++r;
+  }
+
+  while (r < n) {
+    // delete should be first, otherwise
+    // the fixed sized window is not preserved correctly
+    uSet.delete(nums[l]);
+
+    if (uSet.has(nums[r])) {
+      return true;
+    }
+
+    uSet.add(nums[r]);
+
+    ++l;
+    ++r;
+  }
+
+  return false;
+}
+
+var input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  k = 15;
+var out = containsNearbyDuplicate_sliding_window(input, k);
+
+console.log(out);
