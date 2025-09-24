8f83cf869396ca25602f2d56293b9c54da62eaa2
Vipul Dessai <badgamerbad@gmail.com>
2025-09-24 13:48:13 +0530

    dsa

---
+++ src/learn-topic-wise/11-bit-manipulation/006-find-missing.ts
@@ -0,0 +1,21 @@
+﻿export const url =
+  '[Missing Number](https://leetcode.com/problems/missing-number/)';
+
+function missingNumber(nums: number[]): number {
+  const n = nums.length;
+
+  let bits = 0;
+  for (let i = 1; i <= n; ++i) {
+    bits ^= i;
+  }
+  for (let i = 0; i < n; ++i) {
+    bits ^= nums[i];
+  }
+
+  return bits;
+}
+
+var input = [1, 2, 3];
+var out = missingNumber(input);
+
+console.log(out);
