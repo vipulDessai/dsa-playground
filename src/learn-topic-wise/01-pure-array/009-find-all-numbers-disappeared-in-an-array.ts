05048ac16c9cbf440543c1036cf5a4059ac910f7
Vipul Dessai <badgamerbad@gmail.com>
2025-11-11 00:44:59 +0530

    dsa

---
+++ src/learn-topic-wise/01-pure-array/009-find-all-numbers-disappeared-in-an-array.ts
@@ -0,0 +1,15 @@
+export const url =
+  'https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/';
+
+function findDisappearedNumbers(nums: number[]): number[] {
+  const n = nums.length;
+
+  for (let i = 0; i < n; ++i) {}
+
+  return [];
+}
+
+var input = [];
+var out = findDisappearedNumbers(input);
+
+console.log(out);
