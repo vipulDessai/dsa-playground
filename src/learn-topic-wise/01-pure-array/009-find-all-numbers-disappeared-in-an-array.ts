0fc7f48ed8d13b458d4286f6ee151299b0a6d88f
Vipul Dessai <badgamerbad@gmail.com>
2025-11-12 01:10:07 +0530

    dsa

--- src/learn-topic-wise/01-pure-array/009-find-all-numbers-disappeared-in-an-array.ts
+++ src/learn-topic-wise/01-pure-array/009-find-all-numbers-disappeared-in-an-array.ts
@@ -4,12 +4,22 @@ export const url =
 function findDisappearedNumbers(nums: number[]): number[] {
   const n = nums.length;
 
-  for (let i = 0; i < n; ++i) {}
+  for (const num of nums) {
+    const i = Math.abs(num) - 1;
+    nums[i] = -1 * Math.abs(nums[i]);
+  }
 
-  return [];
+  const res: number[] = [];
+  for (let i = 0; i < n; ++i) {
+    if (nums[i] > 0) {
+      res.push(i + 1);
+    }
+  }
+
+  return res;
 }
 
-var input = [];
+var input = [4, 3, 2, 7, 8, 2, 3, 1];
 var out = findDisappearedNumbers(input);
 
 console.log(out);
