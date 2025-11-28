c560836c74b68fe3982deb5138044e0c7e42e64a
Vipul Dessai <badgamerbad@gmail.com>
2025-11-28 19:48:51 +0530

    dsa

---
+++ src/learn-topic-wise/23-cyclic-detection/009-linked-list-cycle.ts
@@ -0,0 +1,25 @@
+﻿export const url =
+  '[Linked List Cycle](https://leetcode.com/problems/linked-list-cycle/)';
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function hasCycle(head: ListNode | null): boolean {
+  let s = head,
+    f = head;
+
+  while (f && f.next) {
+    s = s!.next;
+    f = f.next.next;
+
+    if (f === s) return true;
+  }
+
+  return false;
+}
