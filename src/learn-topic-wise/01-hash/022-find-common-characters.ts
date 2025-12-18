f0abca20b21177d6b94df40b36ca25d080779fec
Vipul Dessai <badgamerbad@gmail.com>
2025-12-18 17:57:44 +0530

    dsa

--- src/learn-topic-wise/01-hash/022-find-common-characters.ts
+++ src/learn-topic-wise/01-hash/022-find-common-characters.ts
@@ -2,29 +2,40 @@ export const url =
   '[Find Common Characters](https://leetcode.com/problems/find-common-characters/)';
 
 function commonChars(words: string[]): string[] {
-  const n = words.length;
-  const wMap: number[] = Array(26).fill(0);
+  const wMap: number[] = Array(26).fill(Infinity);
 
   for (const w of words) {
+    const temp: number[] = new Array(26).fill(0);
     for (let i = 0; i < w.length; ++i) {
-      ++wMap[w.charCodeAt(i) - 'a'.charCodeAt(0)];
+      ++temp[w.charCodeAt(i) - 'a'.charCodeAt(0)];
+    }
+
+    // Update global frequency with minimum counts
+    for (let i = 0; i < 26; i++) {
+      wMap[i] = Math.min(wMap[i], temp[i]);
     }
   }
 
   const res: string[] = [];
   for (let i = 0; i < 26; ++i) {
-    if (wMap[i] >= n) {
-      let count = Math.floor(wMap[i] / n);
-      while (count > 0) {
-        res.push(String.fromCharCode(97 + i));
-        --count;
-      }
+    for (let j = 0; j < wMap[i]; j++) {
+      res.push(String.fromCharCode(97 + i));
     }
   }
   return res;
 }
 
 var input = ['bella', 'label', 'roller'];
+// var input = [
+//   'acabcddd',
+//   'bcbdbcbd',
+//   'baddbadb',
+//   'cbdddcac',
+//   'aacbcccd',
+//   'ccccddda',
+//   'cababaab',
+//   'addcaccd',
+// ];
 var out = commonChars(input);
 
 console.log(out);
