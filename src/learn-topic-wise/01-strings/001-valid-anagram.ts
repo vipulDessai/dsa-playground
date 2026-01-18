f629f0acde71efbf7d11abd9941c9bd6a8119946
Vipul Dessai <badgamerbad@gmail.com>
2026-01-18 22:12:29 +0530

    dsa

--- src/learn-topic-wise/01-strings/001-valid-anagram.ts
+++ src/learn-topic-wise/01-strings/001-valid-anagram.ts
@@ -1,4 +1,5 @@
-export const url = "[Valid Anagram](https://leetcode.com/problems/valid-anagram/)"
+export const url =
+  '[Valid Anagram](https://leetcode.com/problems/valid-anagram/)';
 
 const sortingCallback = (a: string, b: string) => {
   const aLower = a.toLowerCase();
@@ -16,4 +17,27 @@ const checkValidAnagramSort = (s: string, t: string) => {
   );
 };
 
-console.log(checkValidAnagramSort('foo', 'ofo'));
+// console.log(checkValidAnagramSort('foo', 'ofo'));
+
+function isAnagram(s: string, t: string): boolean {
+  const sH = Array(26).fill(0);
+  const tH = Array(26).fill(0);
+
+  for (const c of s) {
+    ++sH[c.charCodeAt(0) - 97];
+  }
+
+  for (const c of t) {
+    ++tH[c.charCodeAt(0) - 97];
+  }
+
+  for (let i = 0; i < 26; ++i) {
+    if (sH[i] !== tH[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(isAnagram('foo', 'ofo'));
