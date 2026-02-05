1320be454d3e518d8ca09edfc756131a4468ffb5
Vipul Dessai <badgamerbad@gmail.com>
2026-02-05 19:55:11 +0530

    vid 0 - sculpt tools plan updated, refactoring

---
+++ src/learn-topic-wise/01-pure-array/020-jump-game.ts
@@ -0,0 +1,10 @@
+export const url = '[Jump Game](https://leetcode.com/problems/jump-game)';
+
+function canJump(nums: number[]): boolean {
+  return true;
+}
+
+var input = [];
+var out = canJump(input);
+
+console.log(out);
