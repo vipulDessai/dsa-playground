f435718fb3a3d51aefeae009bbc28b7d21335b88
Vipul Dessai <badgamerbad@gmail.com>
2026-02-09 14:43:52 +0530

    dsa

--- src/learn-topic-wise/01-pure-array/020-set-matrix-zeroes.ts
+++ src/learn-topic-wise/01-pure-array/020-set-matrix-zeroes.ts
@@ -5,6 +5,21 @@ function setZeroes(matrix: number[][]): void {
   const m = matrix.length,
     n = matrix[0].length;
 
+  let rZ = false,
+    cZ = false;
+  for (let i = 0; i < n; ++i) {
+    if (matrix[0][i] === 0) {
+      rZ = true;
+      break;
+    }
+  }
+  for (let i = 0; i < m; ++i) {
+    if (matrix[i][0] === 0) {
+      cZ = true;
+      break;
+    }
+  }
+
   for (let i = 1; i < m; ++i) {
     for (let j = 1; j < n; ++j) {
       if (matrix[i][j] === 0) {
@@ -16,6 +31,7 @@ function setZeroes(matrix: number[][]): void {
     }
   }
 
+  // fill cols
   for (let i = 1; i < m; ++i) {
     if (matrix[i][0] === 0) {
       for (let j = 1; j < n; ++j) {
@@ -23,8 +39,8 @@ function setZeroes(matrix: number[][]): void {
       }
     }
   }
-
-  for (let i = 0; i < n; ++i) {
+  // fill rows
+  for (let i = 1; i < n; ++i) {
     if (matrix[0][i] === 0) {
       for (let j = 1; j < m; ++j) {
         matrix[j][i] = 0;
@@ -32,12 +48,22 @@ function setZeroes(matrix: number[][]): void {
     }
   }
 
-  if (matrix[0][0] === 0) {
+  if (rZ) {
     for (let i = 0; i < n; ++i) {
       matrix[0][i] = 0;
     }
   }
+  if (cZ) {
+    for (let i = 0; i < m; ++i) {
+      matrix[i][0] = 0;
+    }
+  }
 }
 
-var input = [];
+var input = [
+  [1, 2, 3, 4],
+  [5, 0, 7, 8],
+  [0, 10, 11, 12],
+  [13, 14, 15, 0],
+];
 console.log(setZeroes(input));
