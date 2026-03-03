b0ddadd91ebc48e3adb488a94937e46be9ad7295
Vipul Dessai <badgamerbad@gmail.com>
2026-03-03 10:12:44 +0530

    dsa

--- src/learn-topic-wise/05-prefix-sum/005-incremovable-subarray-count-i.ts
+++ src/learn-topic-wise/05-prefix-sum/005-incremovable-subarray-count-i.ts
@@ -22,9 +22,17 @@ function incremovableSubarrayCount(nums: number[]): number {
   // Try removing every subarray [i..j]
   for (let i = 0; i < n; i++) {
     for (let j = i; j < n; j++) {
-      const leftOk = i === 0 || prefixValid[i - 1];
-      const rightOk = j === n - 1 || suffixValid[j + 1];
+      const leftOk = i === 0 || prefixValid[i - 1]; // check if [0, ..., i - 1] is strictly increasing
+      const rightOk = j === n - 1 || suffixValid[j + 1]; // check if [j + 1, ..., n - 1] is strictly increasing
       if (leftOk && rightOk) {
+        // below condition ensures
+        // 1. [] → empty array is trivially strictly increasing
+        // 2. if only 1 item remains then its a valid strictly increasing subarray
+
+        // also leftOk checks only the left remaining array
+        // similarly rightOk checks only the right remaining array
+        // but we need to check if left remaining last item is strictly < first element
+        // right remainging array
         if (i === 0 || j === n - 1 || nums[i - 1] < nums[j + 1]) {
           count++;
         }
