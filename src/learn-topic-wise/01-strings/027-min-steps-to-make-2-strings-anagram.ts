b5ddb8cb7e4d8221f3a93a6c30eac624a87b5c25
Vipul Dessai <badgamerbad@gmail.com>
2026-03-12 12:15:22 +0530

    dsa

--- src/learn-topic-wise/01-strings/027-min-steps-to-make-2-strings-anagram.ts
+++ src/learn-topic-wise/01-strings/027-min-steps-to-make-2-strings-anagram.ts
@@ -25,3 +25,29 @@ function minSteps(s: string, t: string): number {
 var inS = 'leetcode',
     inT = 'practice';
 console.log(minSteps(inS, inT));
+
+function minSteps_singleMap(s: string, t: string): number {
+    const freq: number[] = new Array(26).fill(0);
+
+    for (let i = 0; i < s.length; i++) {
+        freq[s.charCodeAt(i) - 97]++;
+        freq[t.charCodeAt(i) - 97]--;
+    }
+
+    let steps = 0;
+    for (let i = 0; i < 26; i++) {
+        // since the ask is to replace letters in t
+        // we only care letters frequency count of s > of those in t
+        //
+        // also do use values < 0
+        if (freq[i] > 0) {
+            steps += freq[i];
+        }
+    }
+
+    return steps;
+}
+
+var inS = 'leetcode',
+    inT = 'practice';
+console.log(minSteps_singleMap(inS, inT));
---
+++ src/learn-topic-wise/01-strings/028-determine-if-2-strings-are-close.ts
@@ -0,0 +1,34 @@
+﻿export const url =
+    '[Determine if Two Strings Are Close](https://leetcode.com/problems/determine-if-two-strings-are-close/description/)';
+
+function closeStrings(word1: string, word2: string): boolean {
+    if (word1.length !== word2.length) return false;
+
+    const w1Map = Array(26).fill(0),
+        w2Map = Array(26).fill(0);
+
+    for (let i = 0; i < word1.length; ++i) {
+        w1Map[word1.charCodeAt(i) - 97]++;
+        w2Map[word2.charCodeAt(i) - 97]++;
+    }
+
+    // see if all letter is present in both
+    // atleast 1 letter should be there between the 2 strings
+    for (let i = 0; i < 26; ++i) {
+        if (w1Map[i] > 0 && w2Map[i] === 0) return false;
+
+        if (w2Map[i] > 0 && w1Map[i] === 0) return false;
+    }
+
+    // there should be sufficient letter for transform
+    w1Map.sort((a, b) => a - b);
+    w2Map.sort((a, b) => a - b);
+
+    if (w1Map.join() !== w2Map.join()) return false;
+
+    return true;
+}
+
+var w1 = 'cabbba',
+    w2 = 'abbccc';
+console.log(closeStrings(w1, w2));
