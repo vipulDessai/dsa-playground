0053b564cc94b4982b87788d68f837dc4a1614d3
Vipul Dessai <badgamerbad@gmail.com>
2026-03-26 13:59:38 +0530

    dsa

---
+++ src/learn-topic-wise/01-strings/033-custom-sort-string.ts
@@ -0,0 +1,39 @@
+﻿export const url =
+    '[Custom Sort String](https://leetcode.com/problems/custom-sort-string/)';
+
+function customSortString(order: string, s: string): string {
+    const freq = Array(26).fill(0);
+
+    for (let i = 0; i < s.length; ++i) {
+        ++freq[s.charCodeAt(i) - 97];
+    }
+
+    let res = '';
+    for (let i = 0; i < order.length; ++i) {
+        const index = order.charCodeAt(i) - 97;
+        if (freq[index] > 0) {
+            while (freq[index] > 0) {
+                res += order[i];
+                --freq[index];
+            }
+        }
+    }
+
+    for (let i = 0; i < 26; ++i) {
+        let c = freq[i];
+
+        if (c > 0) {
+            const char = String.fromCharCode(97 + i);
+            while (c > 0) {
+                res += char;
+                --c;
+            }
+        }
+    }
+
+    return res;
+}
+
+var o = 'cba',
+    s = 'abcabcabcddff';
+console.log(customSortString(o, s));
