2fcbbdfd5c51e8913062b3c558ad41f918653e8a
Vipul Dessai <badgamerbad@gmail.com>
2026-03-28 14:12:56 +0530

    dsa

--- src/learn-topic-wise/01-strings/025-expressive-words.ts
+++ src/learn-topic-wise/01-strings/025-expressive-words.ts
@@ -1,85 +1,85 @@
 ﻿export const url =
-  '[Expressive Words](https://leetcode.com/problems/expressive-words/description/)';
+    '[Expressive Words](https://leetcode.com/problems/expressive-words/description/)';
 
 // faster
 function expressiveWords_twoPointers(s: string, words: string[]): number {
-  const isStretchy = (word: string): boolean => {
-    let i = 0,
-      j = 0;
-    while (i < s.length && j < word.length) {
-      if (s[i] !== word[j]) return false;
+    const isStretchy = (word: string): boolean => {
+        let i = 0,
+            j = 0;
+        while (i < s.length && j < word.length) {
+            if (s[i] !== word[j]) return false;
 
-      let lenS = 0,
-        lenW = 0;
-      while (i + lenS < s.length && s[i + lenS] === s[i]) lenS++;
-      while (j + lenW < word.length && word[j + lenW] === word[j]) lenW++;
+            let lenS = 0,
+                lenW = 0;
+            while (i + lenS < s.length && s[i + lenS] === s[i]) lenS++;
+            while (j + lenW < word.length && word[j + lenW] === word[j]) lenW++;
 
-      if (lenS === lenW || (lenS >= 3 && lenW <= lenS)) {
-        i += lenS;
-        j += lenW;
-      } else {
-        return false;
-      }
-    }
+            if (lenS === lenW || (lenS >= 3 && lenW <= lenS)) {
+                i += lenS;
+                j += lenW;
+            } else {
+                return false;
+            }
+        }
 
-    return i === s.length && j === word.length;
-  };
+        return i === s.length && j === word.length;
+    };
 
-  return words.filter(isStretchy).length;
+    return words.filter(isStretchy).length;
 }
 
 var s = 'heeellooo',
-  w = ['hello', 'hi', 'helo'];
+    w = ['hello', 'hi', 'helo'];
 // console.log(expressiveWords_twoPointers(s, w));
 
 // slower
 function expressiveWords_precompute(s: string, words: string[]): number {
-  function group(word: string) {
-    const wordMap: [string, number][] = [];
-    let i = 0;
-    while (i < word.length) {
-      let gLen = 0;
-      while (i < word.length && word[i] === word[i + gLen]) {
-        ++gLen;
-      }
+    function group(word: string) {
+        const wordMap: [string, number][] = [];
+        let i = 0;
+        while (i < word.length) {
+            let gLen = 0;
+            while (i < word.length && word[i] === word[i + gLen]) {
+                ++gLen;
+            }
 
-      wordMap.push([word[i], gLen]);
+            wordMap.push([word[i], gLen]);
 
-      i += gLen;
-    }
-
-    return wordMap;
-  }
-
-  const sMap = group(s),
-    sLen = sMap.length;
-
-  let res = 0;
-  for (const w of words) {
-    const curWordMap = group(w);
-
-    if (curWordMap.length === sLen) {
-      let i = 0;
-      while (i < sLen) {
-        const [sL, sC] = sMap[i];
-        const [wL, wC] = curWordMap[i];
-
-        if (sL !== wL) break;
-
-        if (sC === wC || (sC >= 3 && wC < sC)) {
-          ++i;
-        } else {
-          break;
+            i += gLen;
         }
-      }
 
-      if (i === sLen) ++res;
+        return wordMap;
     }
-  }
 
-  return res;
+    const sMap = group(s),
+        sLen = sMap.length;
+
+    let res = 0;
+    for (const w of words) {
+        const curWordMap = group(w);
+
+        if (curWordMap.length === sLen) {
+            let i = 0;
+            while (i < sLen) {
+                const [sL, sC] = sMap[i];
+                const [wL, wC] = curWordMap[i];
+
+                if (sL !== wL) break;
+
+                if (sC === wC || (sC >= 3 && wC < sC)) {
+                    ++i;
+                } else {
+                    break;
+                }
+            }
+
+            if (i === sLen) ++res;
+        }
+    }
+
+    return res;
 }
 
 var s = 'heeellooo',
-  w = ['hello', 'hi', 'helo'];
+    w = ['hello', 'hi', 'helo'];
 console.log(expressiveWords_precompute(s, w));
---
+++ src/learn-topic-wise/01-strings/034-minimum-cost-to-convert-string-i.ts
@@ -0,0 +1,31 @@
+﻿export const url =
+    '[Count Number of Teams](https://leetcode.com/problems/count-number-of-teams/description/)';
+
+function numTeams(rating: number[]): number {
+    const n = rating.length;
+    let res = 0;
+
+    for (let j = 1; j < n - 1; j++) {
+        let lS = 0,
+            lL = 0,
+            rS = 0,
+            rL = 0;
+
+        for (let i = 0; i < j; ++i) {
+            if (rating[i] < rating[j]) ++lS;
+            else ++lL;
+        }
+
+        for (let k = j + 1; k < n; ++k) {
+            if (rating[k] < rating[j]) ++rS;
+            else ++rL;
+        }
+
+        res += lS * rL + lL * rS;
+    }
+
+    return res;
+}
+
+var input = [2, 5, 3, 4, 1];
+console.log(numTeams(input));
