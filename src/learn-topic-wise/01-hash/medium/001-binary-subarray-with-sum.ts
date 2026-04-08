aac61e8270876b287cc8add6300c58ce49433d8a
Vipul Dessai <badgamerbad@gmail.com>
2026-04-08 16:20:30 +0530

    dsa

---
+++ src/learn-topic-wise/01-hash/medium/001-binary-subarray-with-sum.ts
@@ -0,0 +1,26 @@
+﻿export const url =
+    '[Binary Subarrays With Sum](https://leetcode.com/problems/binary-subarrays-with-sum/description/)';
+
+function numSubarraysWithSum(nums: number[], goal: number): number {
+    const pMap = new Map<number, number>();
+    pMap.set(0, 1);
+
+    let pSum = 0,
+        c = 0;
+    for (const num of nums) {
+        pSum += num;
+
+        const cur = pMap.get(pSum - goal);
+        if (typeof cur === 'number') {
+            c += cur;
+        }
+
+        pMap.set(pSum, (pMap.get(pSum) || 0) + 1);
+    }
+
+    return c;
+}
+
+var nums = [1, 0, 1, 0, 1],
+    g = 0;
+console.log(numSubarraysWithSum(nums, g));
---
+++ src/learn-topic-wise/01-hash/medium/001-largest-subarray-with-k-sum-kadane-algorithm.ts
@@ -0,0 +1,34 @@
+export const url =
+    '[Longest Subarray with Sum K](https://www.geeksforgeeks.org/problems/longest-sub-array-with-sum-k0809/1)';
+
+// find max length of a subarray whose sum is equals to target
+function largestSubArrayKSum(nums: number[], target: number): number {
+    const sumMap = new Map();
+    sumMap.set(0, -1);
+
+    let max = 0,
+        prefixSum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        prefixSum += nums[i];
+
+        if (sumMap.has(prefixSum - target)) {
+            max = Math.max(max, i - sumMap.get(prefixSum - target));
+        }
+
+        // in case of finding only 1 largest subarray,
+        // we store the first index where the current
+        // prefix sum was found for the first time
+        //
+        // checking whether the current prefix sum is stored
+        // or not is important otherwise it will override with the
+        // most recent index, and result will the shortest subarray length
+        if (!sumMap.has(prefixSum)) {
+            sumMap.set(prefixSum, i);
+        }
+    }
+
+    return max;
+}
+
+// console.log(largestSubArrayKSum([5, 1, 4, 3, -4, -1, 8, 23], 8));
+console.log(largestSubArrayKSum([1, -1, 5, -2, 3], 3));
