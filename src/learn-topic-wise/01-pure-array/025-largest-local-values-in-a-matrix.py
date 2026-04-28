cccb5945b0a5dd41827df50c6136dde3dd8d213a
Vipul Dessai <badgamerbad@gmail.com>
2026-04-28 18:02:13 +0530

    dsa: added python debug launch settings

---
+++ src/learn-topic-wise/01-pure-array/025-largest-local-values-in-a-matrix.py
@@ -0,0 +1,48 @@
+﻿# [Largest Local Values in a Matrix](https://leetcode.com/problems/largest-local-values-in-a-matrix/)
+
+class Solution(object):
+    def getMax(self, grid, r, c):
+        """
+        :type grid: List[List[int]]
+        :type r: int
+        :type c: int
+        """
+        _max = -2 ** 31
+        for i in range (r, r + 3):
+            for j in range(c, c + 3):
+                _max = max(_max, grid[i][j])
+        
+        return _max
+
+    def largestLocal(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: List[List[int]]
+        """
+
+        n = len(grid)
+        res = [[0 for _ in range(n - 2)] for _ in range (n - 2)]
+
+        for i in range(0, n - 2):
+            for j in range(0, n - 2):
+                res[i][j] = self.getMax(grid, i, j)
+
+        return res
+    
+def Main():
+    grid = [
+        [9, 9, 8, 1],
+        [5, 6, 2, 6],
+        [8, 2, 6, 4],
+        [6, 2, 2, 2]
+    ]
+    
+    sol = Solution()
+    output = sol.largestLocal(grid)
+
+    for row in output:
+        print(row)
+
+
+if __name__ == "__main__":
+    Main()
--- src/learn-topic-wise/02-sliding-window/021-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ src/learn-topic-wise/02-sliding-window/021-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -2,64 +2,59 @@
 
 #include <iostream>
 #include <vector>
+#include <limits>
+#include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
-namespace _021_length_of_longest_subarray_with_at_most_k_frequency {
-    class Solution {
+namespace _021_length_of_longest_subarray_with_at_most_k_frequency
+{
+    class Solution
+    {
     public:
-        int maxSubarrayLength(vector<int>& nums, int k) {
-            // int n = nums.Length;
-            // Dictionary<int, int> m = new();
-            // int l = 0,
-            //     r = 0;
-            // int c = int.MinValue;
-            // while (r < n)
-            // {
-            //     int cur = nums[r];
+        int maxSubarrayLength(vector<int> &nums, int k)
+        {
+            unordered_map<int, int> freq;
+            int l = 0,
+                r = 0;
+            int res = numeric_limits<int>::min();
+            
+            while (r < nums.size())
+            {
+                int cur = nums[r];
+                ++freq[cur];
 
-            //     if (!m.ContainsKey(cur))
-            //     {
-            //         m[cur] = 0;
-            //     }
+                while(freq[cur] > k)
+                {
+                    --freq[nums[l]];
+                    ++l;
+                }
 
-            //     if (m[cur] < k)
-            //     {
-            //         m[cur]++;
-            //         r++;
+                ++r;
+            }
 
-            //         c = Math.Max(c, r - l);
-            //     }
-            //     else
-            //     {
-            //         int lN = nums[l];
-            //         m[lN]--;
-            //         if (m[lN] == 0)
-            //         {
-            //             m.Remove(lN);
-            //         }
-            //         l++;
-            //     }
-            // }
-
-            // return c;
+            return res;
         }
     };
 };
 
-class Execute {
+class Execute
+{
 public:
-    static void Main() {
+    static void Main()
+    {
         _021_length_of_longest_subarray_with_at_most_k_frequency::Solution obj;
 
         vector<int> input = {};
         int k = 0;
-        
+
         cout << obj.maxSubarrayLength(input, k);
     }
 };
 
-int main() {
+int main()
+{
     Execute::Main();
     return 0;
 }
