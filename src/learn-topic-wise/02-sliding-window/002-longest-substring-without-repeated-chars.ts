b776a406f3d279c3bf84704ca8083ebf08dead6a
Vipul Dessai <badgamerbad@gmail.com>
2026-05-06 14:38:08 +0530

    ts only

--- src/learn-topic-wise/02-sliding-window/002-longest-substring-without-repeated-chars.ts
+++ src/learn-topic-wise/02-sliding-window/002-longest-substring-without-repeated-chars.ts
@@ -1,21 +1,21 @@
 export const url =
-  '[longest substring without repeating characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/description/)';
+    '[longest substring without repeating characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/description/)';
 
 function lengthOfLongestSubstring(s: string): number {
-  const charSet = new Set();
-  let l = 0;
-  let result = 0;
-  for (let r = 0; r < s.length; r++) {
-    while (charSet.has(s[r])) {
-      charSet.delete(s[l]);
-      ++l;
+    const charSet = new Set();
+    let l = 0;
+    let result = 0;
+    for (let r = 0; r < s.length; r++) {
+        while (charSet.has(s[r])) {
+            charSet.delete(s[l]);
+            ++l;
+        }
+
+        charSet.add(s[r]);
+        result = Math.max(result, r - l + 1);
     }
 
-    charSet.add(s[r]);
-    result = Math.max(result, r - l + 1);
-  }
-
-  return result;
+    return result;
 }
 
 console.log(lengthOfLongestSubstring('abcabcbb'));
