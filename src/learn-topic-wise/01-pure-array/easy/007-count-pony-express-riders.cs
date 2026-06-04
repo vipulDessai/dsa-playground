f4f5689dfa60b894db435022a809aa265a4ac484
Vipul Dessai <badgamerbad@gmail.com>
2026-06-04 22:37:34 +0530

    dsa

--- src/learn-topic-wise/01-pure-array/easy/007-count-pony-express-riders.cs
+++ src/learn-topic-wise/01-pure-array/easy/007-count-pony-express-riders.cs
@@ -1,9 +1,9 @@
 ﻿// https://www.codewars.com/kata/5b18e9e06aefb52e1d0001e9/train/javascript
 namespace learning_dsa_csharp._01_strings_arrays_hash._063_count_pony_express_riders
 {
-    internal class MySoln
+    internal class Solution
     {
-        public static int Riders(int[] stations)
+        public static int riders(int[] stations)
         {
             int n = stations.Length;
 
@@ -21,6 +21,17 @@ namespace learning_dsa_csharp._01_strings_arrays_hash._063_count_pony_express_ri
             }
 
             return res + 1;
+        }
+    }
+
+    class Execute
+    {
+        public static void Main(string[] args)
+        {
+            int[] input = [1, 3, 4, 2, 2];
+            var output = Solution.riders(input);
+
+            Console.WriteLine(output);
         }
     }
 }
---
+++ src/learn-topic-wise/07-linked-List/medium/005-remove-zero-sum-consecutive-nodes-from-linked-list.cs
@@ -0,0 +1,131 @@
+﻿using Utils;
+
+namespace learning_dsa_csharp._06_linked_list._011_remove_zero_sum_consecutive_nodes_from_linked_list
+{
+    internal class Solution
+    {
+        public ListNode RemoveZeroSumSublists(ListNode head)
+        {
+            if (head.next == null && head.val == 0)
+            {
+                return null;
+            }
+
+            Dictionary<int, ListNode> m = new Dictionary<int, ListNode>();
+
+            var i = head;
+            int s = 0;
+            while (i != null)
+            {
+                s += (int)i.val;
+
+                if (s == 0)
+                {
+                    head = i.next;
+                    m = new Dictionary<int, ListNode>();
+                }
+                else
+                {
+                    if (m.ContainsKey(s))
+                    {
+                        var n = m[s];
+                        n.next = i.next;
+
+                        m = new Dictionary<int, ListNode>();
+                        s = 0;
+                        var j = head;
+                        while (j != n.next)
+                        {
+                            s += (int)j.val;
+                            m[s] = j;
+                            j = j.next;
+                        }
+                    }
+                    else
+                    {
+                        m[s] = i;
+                    }
+                }
+
+                i = i.next;
+            }
+
+            if (s == 0)
+            {
+                return null;
+            }
+
+            return head;
+        }
+    }
+
+    public class MySoln
+    {
+        public ListNode RemoveZeroSumSublists(ListNode head)
+        {
+            var cur = head;
+
+            Dictionary<int, (ListNode, int)> map = new();
+            map[0] = (null, -1);
+
+            int pSum = 0, i = 0, max = int.MinValue;
+            ListNode s = null, e = null;
+            while (cur != null)
+            {
+                pSum += cur.val;
+
+                if (map.ContainsKey(pSum))
+                {
+                    var (lNode, lInd) = map[pSum];
+
+                    if (max < i - lInd)
+                    {
+                        max = i - lInd;
+                        s = lNode;
+                        e = cur;
+                    }
+                }
+
+                if (!map.ContainsKey(pSum))
+                    map[pSum] = (cur, i);
+
+                cur = cur.next;
+                ++i;
+            }
+
+            if(s == null && e == null)
+            {
+                return head;
+            }
+
+            if (s == null)
+            {
+                head = e.next;
+            }
+            else
+            {
+                s.next = e.next;
+            }
+
+            return head;
+        }
+    }
+
+    class Execute
+    {
+        public static void Main(string[] args)
+        {
+            int[] input = [1, 2, -3, 3, 1];
+            input = [2, 2, -3, 3, 1];
+            ListNode inputList = LinkedListGenerator.Generate(input);
+
+            var output = new MySoln().RemoveZeroSumSublists(inputList);
+
+            while (output != null)
+            {
+                Console.WriteLine(output.val);
+                output = output.next;
+            }
+        }
+    }
+}
