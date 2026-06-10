353589b737ae4e3e92b30d4f9e5aba4f3c36e893
Vipul Dessai <badgamerbad@gmail.com>
2026-06-10 19:16:09 +0530

    dsa

--- src/learn-topic-wise/08-l-0-graphs/medium/002-pacific-atlantic-water-flow.cpp
+++ src/learn-topic-wise/08-l-0-graphs/medium/002-pacific-atlantic-water-flow.cpp
@@ -1,66 +1,61 @@
-﻿#include <iostream>
-#include <set>
+﻿// [Pacific Atlantic Water Flow](https://leetcode.com/problems/pacific-atlantic-water-flow/)
+
+#include <array>
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
 namespace _003_pacific_atlantic_water_flow {
 class Solution {
-    vector<vector<int>> h;
-    int rLength;
-    int cLength;
-
    public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        // auto pac = new HashSet<(int, int)>();
-        // auto atl = new HashSet<(int, int)>();
+        int m = heights.size(), n = heights[0].size();
+        vector<vector<bool>> pacific(m, vector<bool>(n, false));
+        vector<vector<bool>> atlantic(m, vector<bool>(n, false));
 
-        // rLength = heights.Length;
-        // cLength = heights[0].Length;
+        // Run DFS from every border cell, flowing UPHILL
+        for (int i = 0; i < m; i++) {
+            dfs(heights, pacific, i, 0);       // left edge  -> Pacific
+            dfs(heights, atlantic, i, n - 1);  // right edge -> Atlantic
+        }
+        for (int j = 0; j < n; j++) {
+            dfs(heights, pacific, 0, j);       // top edge    -> Pacific
+            dfs(heights, atlantic, m - 1, j);  // bottom edge -> Atlantic
+        }
 
-        // h = heights;
+        // Cells reachable by BOTH oceans are the answer
+        vector<vector<int>> result;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (pacific[i][j] && atlantic[i][j])
+                    result.push_back({i, j});
 
-        // for (int c = 0; c < cLength; c++)
-        // {
-        //     dfs(0, c, pac, h[0][c]);
-        //     dfs(rLength - 1, c, atl, h[rLength - 1][c]);
-        // }
-
-        // for (int r = 0; r < rLength; r++)
-        // {
-        //     dfs(r, 0, pac, h[r][0]);
-        //     dfs(r, cLength - 1, atl, h[r][cLength - 1]);
-        // }
-
-        // var res = new List<IList<int>>();
-        // for (int r = 0; r < rLength; r++)
-        // {
-        //     for (int c = 0; c < cLength; c++)
-        //     {
-        //         if (pac.Contains((r, c)) && atl.Contains((r, c)))
-        //         {
-        //             res.Add(new List<int>() { r, c });
-        //         }
-        //     }
-        // }
-        // return res;
-
-        return heights;
+        return result;
     }
 
    private:
-    void dfs(int r, int c, set<pair<int, int>> visit, int prevHeight) {
-        if (
-            visit.count({r, c}) || r < 0 || c < 0 || r == rLength || c == cLength || h[r][c] < prevHeight) {
-            return;
+    void dfs(vector<vector<int>>& heights, vector<vector<bool>>& visited,
+             int r, int c) {
+        int m = heights.size(), n = heights[0].size();
+        visited[r][c] = true;
+
+        // 4 directions, no diagonals
+        array<array<int, 2>, 4> directions = {{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}};
+
+        for (int d = 0; d < 4; d++) {
+            int nr = r + directions[d][0], nc = c + directions[d][1];
+
+            if (nr < 0 || nr >= m || nc < 0 || nc >= n) continue;
+
+            if (visited[nr][nc]) continue;
+
+            if (heights[nr][nc] < heights[r][c]) continue;
+
+            // Reverse logic: we move to a neighbor only if it's >= current
+            // (because in real flow, water comes DOWN from that higher neighbor)
+            dfs(heights, visited, nr, nc);
         }
-
-        visit.insert({r, c});
-
-        dfs(r + 1, c, visit, h[r][c]);
-        dfs(r - 1, c, visit, h[r][c]);
-        dfs(r, c + 1, visit, h[r][c]);
-        dfs(r, c - 1, visit, h[r][c]);
     }
 };
 }  // namespace _003_pacific_atlantic_water_flow
@@ -68,20 +63,25 @@ class Solution {
 class Execute {
    public:
     static void Main() {
+        // _003_pacific_atlantic_water_flow::Solution s;
         _003_pacific_atlantic_water_flow::Solution s;
 
-        vector<vector<int>> input = {{1, 2, 2, 3, 5}, {3, 2, 3, 4, 4}, {2, 4, 5, 3, 1}, {6, 7, 1, 4, 5}, {5, 1, 1, 2, 4}};
+        vector<vector<int>> input = {
+            {1, 2, 2, 3, 5},
+            {3, 2, 3, 4, 4},
+            {2, 4, 5, 3, 1},
+            {6, 7, 1, 4, 5},
+            {5, 1, 1, 2, 4}};
 
         auto output = s.pacificAtlantic(input);
 
         int rLen = output.size(), cLen = output[0].size();
 
         for (int i = 0; i < rLen; ++i) {
-            for (int j = 0; j < cLen; ++j) {
-                cout << output[i][j] << " ";
-            }
+            int r = output[i][0];
+            int c = output[i][1];
 
-            cout << endl;
+            cout << "[" << r << ", " << c << "] : " << input[r][c] << endl;
         }
     }
 };
