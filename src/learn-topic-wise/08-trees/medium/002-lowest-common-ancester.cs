fdcd2508039944f71448bf08bcd6bc56095790e2
Vipul Dessai <badgamerbad@gmail.com>
2026-07-07 19:10:17 +0530

    dsa

--- src/learn-topic-wise/08-trees/medium/002-lowest-common-ancester.cs
+++ src/learn-topic-wise/08-trees/medium/002-lowest-common-ancester.cs
@@ -6,48 +6,41 @@ namespace learning_dsa_csharp._07_trees._002_lowest_common_ancester
 {
     internal class Solution
     {
-        // LCA is a node which is either common between p or q
-        // or with itself
-        // so that its the farthest from the root node
-        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        private TreeNode dfs(TreeNode n, TreeNode p, TreeNode q)
         {
-            TreeNode checkNode(TreeNode n)
+            if (n == null)
             {
-                if (n == null)
-                {
-                    return null;
-                }
-
-                // because if say suppose one of either p or q is found then
-                // return it, as whichever is remaining and is below the current node then LCS is the current node only
-                //
-                // and only if its not the current then we go one more level down
-                if (n.val == p.val || n.val == q.val)
-                {
-                    return n;
-                }
-
-                var lca1 = checkNode(n.left);
-                var lca2 = checkNode(n.right);
-
-                if (lca1 != null && lca2 != null)
-                {
-                    return n;
-                }
-
-                if (lca1 != null)
-                {
-                    return lca1;
-                }
-                if (lca2 != null)
-                {
-                    return lca2;
-                }
-
                 return null;
             }
 
-            return checkNode(root);
+            if (n == p || n == q)
+            {
+                return n;
+            }
+
+            var l = dfs(n.left, p, q);
+            var r = dfs(n.right, p, q);
+
+            if (l != null && r != null)
+            {
+                return n;
+            }
+
+            if (l != null)
+            {
+                return l;
+            }
+
+            if (r != null)
+            {
+                return r;
+            }
+
+            return null;
+        }
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            return dfs(root, p, q);
         }
     }
 
