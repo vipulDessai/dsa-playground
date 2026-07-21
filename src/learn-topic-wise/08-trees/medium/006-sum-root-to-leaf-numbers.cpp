ddb3324632885fe81b0af3a6183e8de8d869a4a8
Vipul Dessai <badgamerbad@gmail.com>
2026-07-21 19:49:03 +0530

    dsa

--- src/learn-topic-wise/08-trees/medium/006-sum-root-to-leaf-numbers.cpp
+++ src/learn-topic-wise/08-trees/medium/006-sum-root-to-leaf-numbers.cpp
@@ -1,24 +1,46 @@
 ﻿// https://leetcode.com/problems/sum-root-to-leaf-numbers/
 
-// TODO
+#include <optional>
+#include <vector>
+
+#include "generate-trees.h";
+
+using namespace utils;
+
 namespace _010_sum_root_to_leaf_numbers {
-class MySoln {
-   public:
-    int SumNumbers(TreeNode root) {
-        int dfs(TreeNode n, int pS) {
-            if (n != null) {
-                int cur = (pS * 10) + (int)n.val;
-                if (n.left == null && n.right == null)
-                    return cur;
-                else {
-                    return dfs(n.left, cur) + dfs(n.right, cur);
-                }
-            } else {
-                return 0;
-            }
+class Solution {
+   private:
+    int dfs(TreeNode* n, int pS) {
+        if (!n) {
+            return 0;
         }
 
+        int cur = (pS * 10) + n->val;
+        if (n->left == nullptr && n->right == nullptr)
+            return cur;
+
+        return dfs(n->left, cur) + dfs(n->right, cur);
+    }
+
+   public:
+    int SumNumbers(TreeNode* root) {
         return dfs(root, 0);
     }
-}
+};
+
 }  // namespace _010_sum_root_to_leaf_numbers
+
+class Execute {
+   public:
+    static void Main() {
+        _010_sum_root_to_leaf_numbers::Solution s;
+        vector<optional<int>> arr = {1, 2, 3};
+        TreeNode* root = TreeOperations::generate(arr);
+        s.SumNumbers(root);
+    }
+};
+
+int main() {
+    Execute::Main();
+    return 0;
+}
